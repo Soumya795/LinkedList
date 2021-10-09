843b84c652dcf6f7662648e1d442ad03338cb5a5
Soumya795 <soumyasubhakanta97@gmail.com>
2021-10-09 20:06:54 +0530

    Insertion

--- src/LinkedListProblem/Linkedlist.java
+++ src/LinkedListProblem/Linkedlist.java
@@ -1,26 +1,65 @@
 package LinkedListProblem;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-
 public class Linkedlist {
-    public static void main(String[] args) {
-        LinkedList<Integer> s = new LinkedList<>();
-        System.out.println("Welcome to Linked List:");
+    static Node head;
 
-        s.add(70);
+    static class Node {
+        int data;
+        Node next;
 
-        System.out.println("list:" + s);
-        s.addLast(30);
-        s.addLast(56);
-
-        System.out.println("new list:" + s);
-
-        Iterator i = s.descendingIterator();
-        System.out.println("Reversing list:");
-        while (i.hasNext()) {
-            System.out.println(i.next());
+        Node(int d) {
+            data = d;
+            next = null;
         }
+    }
+
+
+    static void insertAtMid(int x) {
+
+        if (head == null)
+            head = new Node(x);
+        else {
+
+            Node newNode = new Node(x);
+
+            Node ptr = head;
+            int len = 0;
+
+            while (ptr != null) {
+                len++;
+                ptr = ptr.next;
+            }
+            int count = ((len % 2) == 0) ? (len / 2) : (len + 1) / 2;
+            ptr = head;
+            while (count-- > 1)
+                ptr = ptr.next;
+
+            newNode.next = ptr.next;
+            ptr.next = newNode;
+        }
+    }
+
+    static void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        head = new Node(56);
+        head.next = new Node(70);
+
+        System.out.println("linked list before insertion:");
+        display();
+
+        int x = 30;
+        insertAtMid(x);
+        System.out.println("linked list after insertion:");
+        display();
+
 
     }
 }
