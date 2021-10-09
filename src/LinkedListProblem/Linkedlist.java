8ccfb7e531cd3d66b654aef918d2ee036ca5827e
Soumya795 <soumyasubhakanta97@gmail.com>
2021-10-09 19:57:47 +0530

    Appending

--- src/LinkedListProblem/Linkedlist.java
+++ src/LinkedListProblem/Linkedlist.java
@@ -1,20 +1,26 @@
 package LinkedListProblem;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Linkedlist {
-    public static void main(String[] args ) {
+    public static void main(String[] args) {
         LinkedList<Integer> s = new LinkedList<>();
         System.out.println("Welcome to Linked List:");
 
-        s.add(56);
+        s.add(70);
 
-        System.out.println("list:"+s);
+        System.out.println("list:" + s);
         s.addLast(30);
-        s.addLast(70);
+        s.addLast(56);
 
-        System.out.println("new list:"+s);
+        System.out.println("new list:" + s);
 
+        Iterator i = s.descendingIterator();
+        System.out.println("Reversing list:");
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
 
     }
 }
