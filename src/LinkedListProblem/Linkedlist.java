9127dbfff2761b7420373b52fc729aaf72c38116
Soumya795 <soumyasubhakanta97@gmail.com>
2021-10-09 19:54:16 +0530

    Adding Numbers

--- src/LinkedListProblem/Linkedlist.java
+++ src/LinkedListProblem/Linkedlist.java
@@ -3,15 +3,17 @@ package LinkedListProblem;
 import java.util.LinkedList;
 
 public class Linkedlist {
-    public static void main(String[] args) {
+    public static void main(String[] args ) {
         LinkedList<Integer> s = new LinkedList<>();
         System.out.println("Welcome to Linked List:");
 
         s.add(56);
-        s.add(30);
-        s.add(70);
 
-        System.out.println(s);
+        System.out.println("list:"+s);
+        s.addLast(30);
+        s.addLast(70);
+
+        System.out.println("new list:"+s);
 
 
     }
